61cf61de7186d2d38014365e0b93835663ad3c32
Bryson Varner <brysonandmellisa@hotmail.com>
2023-08-15 12:21:10 -0500

    Finished Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Net.Security;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            
+            Console.WriteLine("Hello! Would you like to try and guess my favorite number (1 to 1000)?");
+            var userInput = int.Parse(Console.ReadLine());
+
+            
+            
+             if (userInput > favNumber)
+             {
+                    Console.WriteLine("Too high");
+                    Console.WriteLine("Please try again!");
+             }
+
+             else if (userInput < favNumber)
+             {
+                    Console.WriteLine("Too low");
+                    Console.WriteLine("Please try again!");
+             }
+             else
+             {
+                    Console.WriteLine("You Guessed It!");
+             }
+            
+            
         }
     }
 }
